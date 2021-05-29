d1031ca77514ce60056da282f51321c597dc410c
Bukaeva Bayrta <bbembeeva@gmail.com>
2021-05-29 20:14:09 +0300

    fix expected and show issue

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -35,7 +35,7 @@ public class CashbackHackServiceTest {
     public void shouldCalculateIfAmount1000() {
         int amount = 1000;
         int actual = service.remain(amount);
-        int expected = 1000;
+        int expected = 0;
         assertEquals(actual,expected);
     }
 }
